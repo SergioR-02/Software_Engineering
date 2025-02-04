b17cc3b63fc1704a8f4d3ff8bd04c7639a1e172d
Cristian Daniel Montañez <110000225+CristianMontane@users.noreply.github.com>
2025-02-04 14:27:08 -0500

    Refactor login component to use LoginRegistrationForget and streamline error handling

--- Proyecto/frontend/src/templates/login/Login.jsx
+++ Proyecto/frontend/src/templates/login/Login.jsx
@@ -1,25 +1,10 @@
 import './Login.scss'
-import { useState } from 'react'
-import LoginForm from '../../molecules/loginForm/LoginForm'
+import LoginRegistrationForget from '../../organisms/loginRegistrationForget/LoginRegistrationForget'
 
 const Login = () => {
-  const [error, setError] = useState(null);
-
-  const handleLogin = (name, password) => {
-    if (name !== "c@gmail.com" || password !== "123") {
-      setError("Credenciales Incorrectas");
-    } else {
-      setError(null);
-    }
-  };
-
   return (
     <div className='login'>
-      <div className='login__form-container'>
-        <LoginForm onSubmit={handleLogin} error={error} />
-        <a href="#" className='login__forgot-password'>¿Olvidaste tu contraseña?</a>
-        <a href="#" className='login__register'>Registrate</a>
-      </div>
+      <LoginRegistrationForget />
     </div>
   )
 }
