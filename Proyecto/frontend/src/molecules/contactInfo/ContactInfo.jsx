3e3cb409ede3f1ea7d6713ec1591145dcff24457
Sergio Ruiz <118277619+SergioR-02@users.noreply.github.com>
2025-02-04 15:11:51 -0500

    Arreglo de estilos

--- Proyecto/frontend/src/molecules/contactInfo/ContactInfo.jsx
+++ Proyecto/frontend/src/molecules/contactInfo/ContactInfo.jsx
@@ -4,19 +4,18 @@ import Icon from "../../atoms/icon/Icon";
 export const ContactInfo = () => (
   
   <div className="contactInfo">
-      <Icon size={40} name={"logo_Icon"}/>
       <h2 className="contactInfo__title">Contactanos</h2>
     <div className="contactInfo__social">
-      <a href="https://twitter.com" className="styles.socialLink">
+      <a href="https://twitter.com" target="_blank" className="styles.socialLink">
         <span className="icon twitterIcon"></span>
       </a>
-      <a href="https://linkedin.com" className="styles.socialLink">
+      <a href="https://linkedin.com" target="_blank" className="styles.socialLink">
         <span className="icon linkedinIcon"></span>
       </a>
-      <a href="https://github.com" className="styles.socialLink">
+      <a href="https://github.com" target="_blank" className="styles.socialLink">
         <span className="icon githubIcon"></span>
       </a>
-      <a href="https://instagram.com" className="styles.socialLink">
+      <a href="https://instagram.com" target="_blank" className="styles.socialLink">
         <span className="icon instagramIcon"></span>
       </a>
     </div>
