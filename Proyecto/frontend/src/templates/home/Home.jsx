a8e26cd33561dd1933c42df9f683fb79c50343f2
Cristian Daniel Montañez <daniel.montanez@vendly.co>
2025-02-21 16:26:34 -0500

    Add testing configuration and update React imports

--- Proyecto/frontend/src/templates/home/Home.jsx
+++ Proyecto/frontend/src/templates/home/Home.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from '../../organisms/header/Header';
 import LostObjectsSection from '../../organisms/lostObjectsSection/LostObjectsSection';
 import { Footer } from '../../organisms/footer/Footer';
