81cbc6a87ecfa80a3313a24859cb39cac03dad88
DanielMontanez <cmontanez@unal.edu.co>
2025-03-02 18:16:54 -0500

    Implement dynamic object details page with data fetching and rendering

--- Proyecto/frontend/src/templates/objectDetails/ObjectDetails.jsx
+++ Proyecto/frontend/src/templates/objectDetails/ObjectDetails.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ObjectDetails.scss';
 import ObjectCard from '../../organisms/objectCard/ObjectCard';
 import Header from '../../organisms/header/Header';
 import { Footer } from '../../organisms/footer/Footer';
+import { getFilteredObjects } from '../../utilities/getObjectId';
+import { useUserStore } from '../../store/userStore';
+import { useLocation } from 'react-router-dom';
+import dayjs from 'dayjs';
+
 
 export default function ObjectDetails() {
+  const query = new URLSearchParams(useLocation().search);
+  const id = query.get('id');
+  const { userId } = useUserStore();
+  const [object, setObject] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchObject = async () => {
+      try {
+        const data = await getFilteredObjects(userId, id);
+        setObject(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchObject();
+  }, [id]);
+
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+
   return (
     <>
       <Header />
       <div className='object-details-container'>
         <ObjectCard
-          imageSrc='/public/llaves.jpg'
+          imageSrc={`http://localhost:3000/user/${userId}/images/${object.image_url}`}
           imageAlt='Imagen del objeto'
-          title='LLaves de toallin'
-          category='llaves'
-          state='Perdido'
-          location='Biblioteca'
-          date='2023-05-15'
-          // description='llaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencialllaes con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencial'
-          description={
-            'llaves con llaverito de toallin, tiene una llave de la casa y un lector para conjunto residencial'
-          }
-          contactInfo='juan.perez@unal.edu.co'
+          title={object.title}
+          category={object.category}
+          state={object.status}
+          location={object.location}
+          date={dayjs(object.date_lost_or_found).format('DD/MM/YYYY')}
+          description={object.description}
+          contactInfo={object.contact_method}
         />
       </div>
       <Footer />
